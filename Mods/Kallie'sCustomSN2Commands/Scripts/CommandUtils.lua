fc10504e10bde62f2b4561d03f03b89bc07fc60f
Kallie23 <31892011+32Kallies@users.noreply.github.com>
2026-05-14 15:19:54 -0400

    Add new CommandUtils file

---
+++ Mods/Kallie'sCustomSN2Commands/Scripts/CommandUtils.lua
@@ -0,0 +1,39 @@
+local CommandUtils = {}
+local UEHelpers = require("UEHelpers")
+
+---Teleports the players to the given coordinates (in centimeters)
+---@param position FVector
+function CommandUtils.TeleportPlayer(position)
+    local player = UEHelpers:GetPlayerController()
+    player.Pawn:K2_TeleportTo(position, {})
+    player.Pawn:K2_SetActorLocation(position, false, {}, false)
+    print(string.format(
+        "Teleported player to (%.0f, %.0f, %.0f) for goto command\n",
+        position.X,
+        position.Y,
+        position.Z
+    ))
+end
+
+---Loads a class by its path, even if it wasn't previously loaded into memory.
+---Not sure if a better alternative exists.
+---@return UClass
+---@param path string
+function CommandUtils.LoadClassByPath(path)
+    print("Loading entity by path " .. path .. "\n")
+    local UEHelpers = require("UEHelpers")
+    local load = StaticFindObject("/Script/Engine.KismetSystemLibrary:LoadClassAsset_Blocking")
+    local kismet_library = StaticFindObject("/Script/Engine.KismetSystemLibrary")
+    local make_soft = StaticFindObject("/Script/Engine.KismetSystemLibrary:MakeSoftClassPath")
+    local convert = StaticFindObject("/Script/Engine.KismetSystemLibrary:Conv_SoftClassPathToSoftClassRef")
+
+    local soft_class_path = make_soft(kismet_library, path)
+    local pointer = convert(kismet_library, soft_class_path)
+    local world = UEHelpers.GetWorld()
+
+    local loaded = load(world, pointer)
+    print("Loaded asset: " .. tostring(loaded) .. "\n")
+    return loaded
+end
+
+return CommandUtils
