7db88c9963d0aaf311476db4f3d64f6a63d8ffc9
Kallie23 <31892011+32Kallies@users.noreply.github.com>
2026-05-15 00:38:39 -0400

    Command enabler mod cleanup

--- Mods/SN2CommandsEnablerMod/Scripts/main.lua
+++ Mods/SN2CommandsEnablerMod/Scripts/main.lua
@@ -7,22 +7,13 @@ NotifyOnNewObject("/Game/Blueprints/UI/HUD/WBP_TopHud.WBP_TopHud_C", function()
 
     print("Loading in game. Preparing hooks for commands.\n")
 
-    Pre1, Post1 = RegisterHook("/Script/UWEUtilities.UWEBuildInfo:IsDevelopmentBuild", function(Context)
+    RegisterHook("/Script/UWEUtilities.UWEBuildInfo:IsDevelopmentBuild", function(Context)
         return true
     end)
 
-    Pre2, Post2 = RegisterHook("/Script/UWEUtilities.UWEBuildInfo:IsFinalShippingBuild", function(Context)
+    RegisterHook("/Script/UWEUtilities.UWEBuildInfo:IsFinalShippingBuild", function(Context)
         return false
     end)
-
-    --[[
-    -- This doesn't even prevent the crash sadly
-    RegisterHook("/Game/Blueprints/UI/Lobby/WBP_SaveBeforeExitScreen.WBP_SaveBeforeExitScreen_C:ExitGame", function(Context)
-        print("ON QUIT\n")
-        UnregisterHook("/Script/UWEUtilities.UWEBuildInfo:IsDevelopmentBuild", Pre1, Post1)
-        UnregisterHook("/Script/UWEUtilities.UWEBuildInfo:IsFinalShippingBuild", Pre2, Post2)
-        end)
-    ]]
 
     print("Subnautica 2 commands enabled.\n")
 
