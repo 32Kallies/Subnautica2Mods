664de7365dccc7e7738f05e654e66f0d6e75a984
Kallie23 <31892011+32Kallies@users.noreply.github.com>
2026-05-18 15:00:44 -0400

    Rename print -> log

--- Mods/CopperFromScanning/Scripts/main.lua
+++ Mods/CopperFromScanning/Scripts/main.lua
@@ -7,12 +7,12 @@ local version = "1.0.0"
 local UEHelpers = require("UEHelpers")
 
 ---@param message string
-local function modPrint(message)
+local function log(message)
     print(string.format("[%s v%s] %s\n", mod_name, version, message))
 end
 
 ---@param message string
-local function debugPrint(message)
+local function debugLog(message)
     if DEBUG then
         print(string.format("[%s v%s DEBUG] %s\n", mod_name, version, message))
     end
@@ -58,13 +58,13 @@ end
 ---@param path string
 local function addItemToInventory(path)
     ExecuteInGameThread(function ()
-        debugPrint("Loading asset by path " .. path)
+        debugLog("Loading asset by path " .. path)
 
         -- This may take a moment
         local item_class = loadClassByPath(path)
 
         if not item_class then
-            modPrint("Failed to load asset by path " .. path)
+            log("Failed to load asset by path " .. path)
             return
         end
 
@@ -72,28 +72,28 @@ local function addItemToInventory(path)
         local local_inventory = SN2Statics_GetLocalPlayerInventory(SN2Statics, SN2Statics)
 
         if not local_inventory then
-            modPrint("Local inventory not found")
+            log("Local inventory not found")
             return
         end
 
     
         if local_inventory:IsFull() then
-            modPrint("Inventory is full")
+            log("Inventory is full")
             return
         end
 
-        debugPrint("Loaded asset: " .. tostring(item_class))
+        debugLog("Loaded asset: " .. tostring(item_class))
         local pos = { X = 0, Y = 0, Z = 0}
         local actor = UEHelpers.GetWorld():SpawnActor(item_class, pos, {})
 
         if not actor then
-            modPrint("Failed to spawn actor")
+            log("Failed to spawn actor")
             return
         end
 
         local_inventory:Pickup(actor, true)
 
-        debugPrint("Item successfully given on scan")
+        debugLog("Item successfully given on scan")
     end)
 end
 
@@ -102,7 +102,7 @@ end
 -- Hook #1: Check if the item that is currently being scanned is already unlocked, then store that information for later
 Pre1, Post1 = RegisterHook("/Script/UWEScanner.UWEScannedActorsComponent:GetActorInstanceScannedProgressForPlayer",
 function(Context, ScannedActor)
-    debugPrint("Querying if should give copper")
+    debugLog("Querying if should give copper")
 
     ---@type UUWEScannedActorsComponent
     local component = Context:get();
@@ -114,15 +114,15 @@ function(Context, ScannedActor)
     local scan_data = UWEScanData_GetScanDataForActor(UWEScanData, scanned_actor)
     
     if not scan_data then
-        debugPrint("Failed to find valid ScanData")
+        debugLog("Failed to find valid ScanData")
         return
     end
 
     scanning_completed_object = component:IsScanDataProgressComplete(scan_data)
 
     if DEBUG then
-        debugPrint("Currently scanning: " .. scan_data.Name:ToString())
-        debugPrint("Complete: " .. tostring(scanning_completed_object))
+        debugLog("Currently scanning: " .. scan_data.Name:ToString())
+        debugLog("Complete: " .. tostring(scanning_completed_object))
     end
 end
 )
@@ -130,25 +130,25 @@ end
 -- Hook #2: When an object is fully scanned, check if its state was already marked as complete, then give the player copper
 Pre2, Post2 = RegisterHook("/Script/UWEScanner.UWEScannedActorsComponent:ClientNotifyScannedInstanceCompleted",
 function(Context, ScannedActor)
-    debugPrint("On scan complete")
+    debugLog("On scan complete")
 
     ---@type AActor
     local scanned_actor = ScannedActor:get()
 
-    debugPrint("Scanned " .. scanned_actor:GetFName():ToString())
+    debugLog("Scanned " .. scanned_actor:GetFName():ToString())
 
     ---@type UUWEScanData
     local scan_data = UWEScanData_GetScanDataForActor(UWEScanData, scanned_actor)
 
     if DEBUG then
-        debugPrint("Scan Data: " .. tostring(scan_data))
-        debugPrint("Name: " .. scan_data.Name:ToString())
-        debugPrint("Note: " .. scan_data.DeveloperNote:ToString())
-        debugPrint("Num required: " .. tostring(scan_data.NumRequired))
+        debugLog("Scan Data: " .. tostring(scan_data))
+        debugLog("Name: " .. scan_data.Name:ToString())
+        debugLog("Note: " .. scan_data.DeveloperNote:ToString())
+        debugLog("Num required: " .. tostring(scan_data.NumRequired))
     end
 
     if scanning_completed_object then
-        modPrint("Giving item on scan")
+        log("Giving item on scan")
         addItemToInventory(item_on_scanning)
     end
 
@@ -156,4 +156,4 @@ function(Context, ScannedActor)
     scanning_completed_object = false
 end)
 
-modPrint("All hooks registered!")
+log("All hooks registered!")
